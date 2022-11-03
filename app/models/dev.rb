428e1eeb93385f5eae703a3c20d82eb8414ffee1
moringa <oscarokumu47@gmail.com>
2022-11-03 18:29:48 +0300

    Done

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,11 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
-end
+    def received_one?(item_name)
+        self.freebies.find do |freebie|
+            freebie.name == item_name
+        end
+    end
+
+    
